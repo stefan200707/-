d26d131110db38ebfd54c326cd2da8229fe2483b
Stefan200707 <prostonet690@gmail.com>
2025-09-12 08:33:26 +0300

    Update

---
+++ seminar_2.cpp
@@ -0,0 +1,14 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+int main() {
+    double pi = 3.1415926535;
+    cout << fixed << setprecision(3);
+    cout << pi << endl;
+    char t = '6';
+    cout << "Привет мир" << endl;
+    string s;
+    getline(cin,s);
+    cout << s << endl;
+    return 0;
+}
